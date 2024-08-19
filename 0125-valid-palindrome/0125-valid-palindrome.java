bcf220c7278252cec8de7b57754c930c51786001
Kausher Imam <86553740+Kausher76@users.noreply.github.com>
2024-08-19 17:14:12 +0530

    Time: 136 ms (5.61%), Space: 46 MB (9.69%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String str = "";
+        for(int i = s.length()-1; i >= 0; i--){
+            str = str + s.charAt(i);
+        }
+        return s.equals(str);
+ 
+    }
+}

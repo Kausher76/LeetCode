c494c2ee28a8f5e6f93f7ddd20116c40e8629604
Kausher Imam <86553740+Kausher76@users.noreply.github.com>
2024-08-20 23:23:52 +0530

    Time: 2 ms (82.35%), Space: 46 MB (48.96%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,14 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int N = nums.length;
-        for(int i = 0; i < N - 1; i++) {
-            for(int j = i + 1; j < N; j++) {
-                if(nums[i] == 0 && nums[j] != 0) {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
+     public void moveZeroes(int[] nums) {
+        int snowBallSize = 0; 
+        for (int i=0;i<nums.length;i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
+            }
+            else if (snowBallSize > 0) {
+	            int t = nums[i];
+	            nums[i]=0;
+	            nums[i-snowBallSize]=t;
             }
         }
     }
